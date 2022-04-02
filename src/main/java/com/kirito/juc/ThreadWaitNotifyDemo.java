94b4b9904a018d032a630f9822ecbb4b1e272081
kirito <1350221894@qq.com>
2022-04-02 21:58:43 +0800

    Initial commit

---
+++ src/main/java/com/kirito/juc/ThreadWaitNotifyDemo.java
@@ -0,0 +1,20 @@
+package com.kirito.juc;
+
+/**
+ * 题目：现在两个线程，可以操作初始值为0的一个变量，
+ * 实现一个线程对该变量+1，一个线程对该变量-1，
+ * 实现交替，求10轮，变量初始值为0
+ *
+ *
+ * 1、  高内聚低耦合前提下，线程操作资源类
+ * 2、  判断|干活|通知
+ */
+public class ThreadWaitNotifyDemo {
+
+}
+
+
+
+class AirConditioner{  //资源类
+
+}
